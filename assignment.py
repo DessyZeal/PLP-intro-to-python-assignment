6b811e82799cddcd431f3bba4b0d86d3a445ae38
Ude Desire <udesire20@gmail.com>
2025-08-15 13:03:46 +0100

    new commit

---
+++ assignment.py
@@ -0,0 +1,9 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+operator = input("Enter the operation (+, -, *, /):")
+if operator == "+":
+    result = int(num1) + int(num2)
+elif operator == "-":
+    result = int(num1)
+
+print("The result is:", result)
